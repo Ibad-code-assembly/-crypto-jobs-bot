7725e6b2fa8fde778e26a8a1c00b4b70f5754c29
Ibad-code-assembly <muhammadibkhan54@gmail.com>
2026-07-08 11:49:47 -0700

    Fix: Remove imports of deleted modules in __init__.py files. Clean up scraper/__init__.py to prevent ModuleNotFoundError on startup.

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,17 +1,3 @@
-from .web3_career import Web3CareerScraper
-from .cryptojobslist import CryptoJobsListScraper
-from .cryptocurrencyjobs import CryptocurrencyJobsScraper
-from .crypto_jobs import CryptoJobsScraper
-from .cryptojobs import CryptoJobsScraper as CryptoJobsComScraper
-from .remote3 import Remote3Scraper
-from .coinmarketcap_jobs import CoinMarketCapJobsScraper
+# Scraper package
+# Note: Scrapers are imported directly in scheduler.py, not through this __init__.py
 
-__all__ = [
-    "Web3CareerScraper",
-    "CryptoJobsListScraper",
-    "CryptocurrencyJobsScraper",
-    "CryptoJobsScraper",
-    "CryptoJobsComScraper",
-    "Remote3Scraper",
-    "CoinMarketCapJobsScraper",
-]
